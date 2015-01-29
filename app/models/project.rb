817fbade06220443c986e17a43f12c1df66e69bd
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-01-29 17:31:19 -0500

    fixed typo

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
 	
 	belongs_to :user
-	has_and_belongs_to_many :projet_application
+	has_and_belongs_to_many :project_application
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,13 @@ class User < ActiveRecord::Base
 
   has_many :projects
   has_one :student_setting
+  
   accepts_nested_attributes_for :student_setting
   has_and_belongs_to_many :project_applications
+
   has_one :professor_setting
   accepts_nested_attributes_for :professor_setting
+
   has_one :student_course
   accepts_nested_attributes_for :student_course
 
