59cbb58eb69253c7798d426950f74e3c50a0bb64
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-06-15 23:01:25 -0400

    added student settings

--- app/controllers/requirement_categories_controller.rb
+++ app/controllers/requirement_categories_controller.rb
@@ -29,14 +29,37 @@ class RequirementCategoriesController < ApplicationController
 	      	value = params[:direction].to_i + (params[:pageNumber]).to_i*10 - 10
 
 	      	if value < @Category_length
-
 		      	@category = RequirementCategory.order(created_at: :desc).offset(value).take(10)
-
 		        render :partial => "users/partials/category/categoryPosts"
-		    else 
 
+		    else 
 		    	render :nothing => true, :status => 200, :content_type => 'text/html'
 
+		    end
+	      }
+	    end
+
+	end
+
+	def flip_students
+		@student_attributes = RequirementCategory.find_by_sql(
+			"SELECT DISTINCT requirement_categories.category_name FROM requirement_categories
+			INNER JOIN requirement_subcategories on
+			requirement_categories.id = requirement_subcategories.requirement_category_id AND
+			requirement_subcategories.student_attribute = '1'
+			ORDER BY requirement_categories.created_at desc")
+
+		attribute_length = @student_attributes.length
+		value = params[:direction].to_i + (params[:pageNumber]).to_i*10 - 10
+
+		respond_to do |format|
+	      format.html {
+	      	puts value
+	      	if value < attribute_length
+		      	@category = @student_attributes[value .. value + 10]
+		        render :partial => "users/partials/category/categoryPosts"
+		    else 
+		    	render :nothing => true, :status => 200, :content_type => 'text/html'
 		    end
 
 	      }
@@ -48,17 +71,13 @@ class RequirementCategoriesController < ApplicationController
 
 		respond_to do |format|
 	      format.html {
-
 	      	puts params[:category_id]
-
 			@subcategory = RequirementSubcategory.where(requirement_category_id: 
 						params[:category_id].to_i)
 
 	      	render :partial => 'requirement_subcategories/partial/panel_listing'
-
 	      }
 	    end		
 
 	end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,6 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!, :find_user
 
 	def home
-		@student_attributes = RequirementCategory.find_by_sql(
-			"SELECT DISTINCT requirement_categories.category_name FROM requirement_categories
-			INNER JOIN requirement_subcategories on
-			requirement_categories.id = requirement_subcategories.requirement_category_id AND
-			requirement_subcategories.student_attribute = '1'
-			ORDER BY requirement_categories.created_at desc")
 
 	end
 
