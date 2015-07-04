131ba15695bdb2144cb8c6d94e1332a5e1203ef8
Christina <christina@artlocalapp.com>
2015-07-04 14:32:07 -0400

    fixed dcsupp not defined error

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,6 @@
 angular.module('dcsupp', ['ui.router', 'templates']).config([
     '$stateProvider', '$urlRouterProvider', '$locationProvider',
     function ($stateProvider, $urlRouterProvider, $locationProvider) {
-        $stateProvider.state('/', {
-            url: '/?:goto',
-            controller: 'IndexCtrl'
-        });
 
         $urlRouterProvider.otherwise('intro');
         $locationProvider.html5Mode(true).hashPrefix('!');
--- app/assets/javascripts/factory.js
+++ app/assets/javascripts/factory.js
@@ -1,3 +1,3 @@
-angular.module('dscsupp').factory('callRequest', function clientIdFactory() {
-  return 'a12345654321x';
-});
+//angular.module('dscsupp').factory('callRequest', function clientIdFactory() {
+//  return 'a12345654321x';
+//});
