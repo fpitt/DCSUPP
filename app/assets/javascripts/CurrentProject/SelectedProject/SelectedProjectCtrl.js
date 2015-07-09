365c63dbaca5a41d44979db1c63a1510b0be172d
Christina <christina@artlocalapp.com>
2015-07-08 22:50:54 -0400

    selected projected page loaded with fake info

--- app/assets/javascripts/CurrentProject/SelectedProject/SelectedProjectCtrl.js
+++ app/assets/javascripts/CurrentProject/SelectedProject/SelectedProjectCtrl.js
@@ -1,3 +1,14 @@
 angular.module('dcsupp').controller('SelectedProjectCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
-    $scope.page = "project";
+    $scope.project = {title: "Some Project",
+        text: "Mom follows Christy Plunkett (Anna Faris), " +
+        "a single mother who, after dealing with her battle with alcoh" +
+        "olism and drug abuse, decides to restart her life in Sonoma, California," +
+        " working as a waitress and attending Alcoholics Anonymous meetings. Her mother B" +
+        "onnie Plunkett (Allison Janney) is also a recovering drug and alcohol addict. C" +
+        "hristy's 17-year-old daughter Violet (Sadie Calvano), who was born when Christy wa" +
+        "s 16, has herself become a teen mother by her boyfriend Luke. Christy also has a young" +
+        "er son Roscoe (Blake Garrett Rosenthal) by her ex-husband Baxter (Matt L. Jones), a deadb" +
+        "eat but likeable pothead. The show, as it progresses, adds themes of real-life issues suc" +
+        "h as alcoholism, teen pregnancy, cancer, homelessness, gambling addiction, domestic violence" +
+        ", death, drug addiction and relapse."};
 }]);
