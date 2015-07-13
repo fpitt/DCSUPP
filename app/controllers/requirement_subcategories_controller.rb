2540d4813e2ae15cb9df16595ee3d636cdedf576
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-07-13 11:48:36 -0400

    can list subcategories

--- app/controllers/requirement_subcategories_controller.rb
+++ app/controllers/requirement_subcategories_controller.rb
@@ -9,6 +9,7 @@ class RequirementSubcategoriesController < ApplicationController
 		respond_to do |format|
 	    	format.json {
 	    		if @category.save
+	    			puts "hi"
 	  				render :json => @category
 	  			else
 	  				render :nothing => true, :status => 200, :content_type => 'text/html'
