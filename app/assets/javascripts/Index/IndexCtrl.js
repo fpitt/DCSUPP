7b1c5cefbb3342c90574fbb62db407915bfbf45a
Christina <christina@artlocalapp.com>
2015-07-15 16:18:01 -0400

    fixed url parse

--- app/assets/javascripts/Index/IndexCtrl.js
+++ app/assets/javascripts/Index/IndexCtrl.js
@@ -4,12 +4,25 @@ angular.module('dcsupp').controller('IndexCtrl', ['$stateParams', '$scope', '$st
 
         var path = $stateParams.goto.split('/');
         var state = path[0];
+        var state2 = path[0];
         var i;
         for (i = 1; i < path.length; i++) {
             state = state + '.' + path[i];
+            if (i != path.length - 1) {
+                state2 = state;
+            };
         };
 
-        $state.go(state);
+        try {
+            $state.go(state);
+        } catch (err) {
+            try {
+                $state.go(state2, {id: path[path.length - 1]});
+            } catch (err2) {
+            }
+            ;
+        }
+        ;
     } else {
         $state.go('intro');
     };
--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,5 @@ Rails.application.routes.draw do
   get "/project_create" => 'projects#create'
 
   get "/*path" => redirect("/?goto=%{path}")
+
 end
