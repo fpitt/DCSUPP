c02e5ed46a1c1ed1574da91be55a8ce50b40496b
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-07-20 17:26:39 -0400

    added authentication

--- app/assets/javascripts/Intro/IntroCtrl.js
+++ app/assets/javascripts/Intro/IntroCtrl.js
@@ -1,5 +1,6 @@
 angular.module('dcsupp').controller('IntroCtrl', ['$scope', '$state', function ($scope, $state) {
     $scope.page = "intro page";
 
+    console.log($.cache)
     $state.go('intro.1')
 }]);
