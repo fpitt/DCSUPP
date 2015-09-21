13228a9c888902141f34b117fed5d8252eb045a7
Alan <alan.yuan@mail.utoronto.ca>
2015-09-21 00:09:13 -0400

    changes in layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Project_list = []
-
-(1..20).each do |i|
-	title = i.to_s + "project"
-	text = "text"
-	Project_list.push([title, text])
-end
-
-Project_list.each do |title, text|
-	Project.create(title: title, text: text)
-end
-
 user = User.create(name: "admin")
 user.administrator = true
 user.save
 
-user2 = User.create(name: "prof")
-user2.professor = true
-user2.save
+#List of Students
+user1 = User.create(name: "Christina Chung")
+user2 = User.create(name: "Alan Yuan")
+user3 = User.create(name: "Jackie Yan")
+user4 = User.create(name: "Harun Mustafa")
+user5 = User.create(name: "Fred Zhou")
+user6 = User.create(name: "Michael Wu")
+user7 = User.create(name: "Han Qiu")
+user8 = User.create(name: "Dave Yungoo Kim")
+user9 = User.create(name: "Tina Park")
+user10 = User.create(name: "Tim Walker")
+user11 = User.create(name: "Anh Phuong Tran")
+user12 = User.create(name: "Jialin Song")
+user13 = User.create(name: "Francesco Gramano")
+user14 = User.create(name: "Jerry Chen")
+user15 = User.create(name: "Daniela Chang")
+user16 = User.create(name: "Krista Martono")
+user17 = User.create(name: "Max Yang")
+user18 = User.create(name: "Teresa Liang")
+user19 = User.create(name: "Katie Lo")
+user20 = User.create(name: "Zoe Song")
+
