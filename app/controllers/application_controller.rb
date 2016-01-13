de5f18153cee7787edbbbfa1bf9ac9122520569a
Alan <alan.yuan@mail.utoronto.ca>
2016-01-13 17:47:13 -0500

    1/6 updates done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,11 @@ class ApplicationController < ActionController::Base
         def authenticate
 
             authenticate_or_request_with_http_basic do |username|
-                user = User.find_or_create_by(utor_id: username)
-                user.name = username
-                #Initially set the User name to the UTOR ID, but change after
+                user = User.find_or_create_by(name: username)
+                #NOTE:
+                #The site was first deployed with name column storing the UTOR_ID, however
+                #future versions required the name of the individual and the column utor_id
+                #is assigned as the "name" of the user.
                 user.save
                 @current_user = user
             end
